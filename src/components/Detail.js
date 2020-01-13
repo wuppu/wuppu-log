cc77dc9f29e6b39de323e9df085221cf38c484a4
Wuppu <wndud1640@gmail.com>
2020-01-13 22:44:26 +0900

    Detail title error - background title(absolute position) is overflow)

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Detail.css";
 
-function Detail({title, content}) {
+function Detail({ title, content }) {
   return (
-    <div>
+    <div className="detail">
       <div className="detail__title">
-        <h1 className="detail__bg_title">{title}</h1>
+        <div className="detail__bg_title">
+          <h1>{title}</h1>
+        </div>
         <h2 className="detail__main_title">{title}</h2>
       </div>
-      <div className="detail__content">
-          {content}
-      </div>
+      <div className="detail__content">{content}</div>
     </div>
   );
 }
