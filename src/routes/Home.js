a068916734698e72d2ec719635ad595290ce38c1
Wuppu <wndud1640@gmail.com>
2020-01-22 01:14:44 +0900

    add image file, update font color, family

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Detail from "../components/Detail";
 
 function Home() {
-  return <Detail title="HOME" img="Algorithm_img.jpg"/>;
+  return <Detail title="HOME" img="Home_img.jpg"/>;
 }
 
 export default Home;
